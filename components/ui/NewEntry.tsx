dc2f547bcafb7634879d8e5373bc7cf87aaac4ca
lagunasdaniel001 <lagunasdaniel001@gmail.com>
2023-06-19 14:38:47 -0500

    Validacion en el formulario y estado del mismo

---
+++ components/ui/NewEntry.tsx
@@ -0,0 +1,68 @@
+import {useState, ChangeEvent} from 'react'
+import {Box, Button, TextField} from '@mui/material'
+import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
+import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
+import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
+export const NewEntry = () => {
+  const [isAdding, setIsAdding] = useState(false)
+
+  const [inputValue, setInputValue] = useState('')
+  const [touched, setTouched] = useState(false)
+
+  const onTextFieldChange = (event:ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value)
+  }
+
+  const onCanceled = () => {
+    setIsAdding(false)
+    setTouched(false)
+  }
+  const onSave = () => {
+    if( inputValue.length <=0) return;
+
+    console.log(inputValue)
+  }
+  return (
+    <Box sx={{ paddingX:3}}>
+        {
+          isAdding 
+          ?(
+            <>
+              <TextField
+                fullWidth
+                sx={{ marginTop:2, marginBottom:1}}
+                placeholder='Nueva Entrada'
+                autoFocus
+                multiline
+                label='Nueva Entrada'
+                helperText={ inputValue.length <=0 && touched && 'Ingrese un valor'}
+                error={ inputValue.length <=0 && touched}
+                value={inputValue}
+                onChange={ onTextFieldChange }
+                onBlur={() => setTouched(true)}
+              />
+              <Box display={'flex'} justifyContent={'space-between'}>
+                  <Button variant="outlined" color='error' endIcon={<CancelOutlinedIcon/>} onClick={onCanceled}> 
+                      Cancelar
+                  </Button>
+                  <Button variant="outlined" color="secondary" endIcon={<SaveOutlinedIcon/>} onClick={onSave}> 
+                      Guardar
+                  </Button>
+              </Box>
+            </>
+          ) 
+          :(
+            <Button
+                variant='outlined'
+                fullWidth
+                endIcon={<AddBoxOutlinedIcon/>}
+                onClick={() => setIsAdding(true)}
+            >
+                Agregar Entrada
+            </Button>
+          )
+        }
+        
+    </Box>
+  )
+}
