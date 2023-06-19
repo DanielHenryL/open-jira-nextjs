2cca8c3eb64efe4a19e11d72263c849a080f8468
lagunasdaniel001 <lagunasdaniel001@gmail.com>
2023-06-18 22:18:03 -0500

    Diseño de pantalla principal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,27 @@
 import { Layout } from "@/components/layouts";
-import { Typography } from "@mui/material";
+import { Typography, Grid, Card, CardHeader } from "@mui/material";
 
 
 export default function Home() {
   return (
     <Layout title="OpenJira">
-      <Typography variant="h3" color='primary'>Hola Mundo</Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{height:'calc(100vh - 100px)'}}> 
+            <CardHeader title='Pendientes'/>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{height:'calc(100vh - 100px)'}}>
+            <CardHeader title='En Progreso'/>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{height:'calc(100vh - 100px)'}}>
+            <CardHeader title='Terminadas'/>
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
